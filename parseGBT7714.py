01a1f7cdfaf3bac3382ac8445f6a63be2d661e8a
ElunDai <daielun@outlook.com>
2019-06-07 16:45:26 +0800

    fix bug and add some pattern

--- parseGBT7714.py
+++ parseGBT7714.py
@@ -3,10 +3,9 @@
 # -*- coding: utf-8 -*-
 #==============================
 #    Author: Elun Dai
-#    Last modified: 2019-06-07 15:44
+#    Last modified: 2019-06-07 16:44
 #    Filename: parseGBT7714.py
 #    Description:
-#    
 #=============================#
 import re
 from collections import defaultdict
@@ -14,14 +13,33 @@ from collections import defaultdict
 useras = {
     'J' : ['author.title[usera].translator,year,volume(number):pages[urldate].url.doi',
            'author.title[usera].translaotr,year,volume(number):pages.url'],
-    "M" : ['author.tittle[usera].tittle[usera].location:publisher'],
+    'M' : ['author.tittle[usera].tittle[usera].location:publisher.'],
     'R' : ['author.title[usera].location:publisher,date.',
            'author.title:subtittle[usera].location:publisher,date.'],
+    'EB/OL' : ['author.title[usera].address,date.',
+                'author.title[usera].address,date/urldate.',],
+    'N' : ['author.title[usera].journaltitle,date(number).',],
+    'D' : ['author.title[usera].address:publisher,year'],
 }
 
 entrytypes = {
+    'M' : 'book',
     'J' : 'article',
+    'C' : 'proceedings',
+    'G' : 'collection',
+    'N' : 'newspaper',
+    'D' : 'mastersthesis',
     'R' : 'report',
+    'EB/OL' : 'online',
+    'EB' : 'online',
+    'S' : 'standard',
+    'P' : 'patent',
+    'DB' : 'database',
+    'CP' : 'software',
+    'A' : 'archive',
+    'CM' : 'map',
+    'DS' : 'dataset',
+    'Z' : 'misc',
 }
 
 for usera in useras:
@@ -57,17 +75,20 @@ def parse(s):
     print("parsing", s)
     usera = getusera(s)
     parsed_dict['usera'] = usera
-    for style in useras.get(usera):
+    styles = useras.get(usera)
+    if styles is None:
+        raise IndexError("Couldn't find pattern of entry file " + usera)
+    for style in styles:
         pattern, files = str2pattern(style)
         res = re.match(pattern, s)
         if res is not None:
             res = res.groups()
             assert len(files) == len(res)
             parsed_dict.update(dict(zip(files, res)))
-            break
-    print("matched!", style)
-    print(len(parsed_dict)-1, "files has found!")
-    return parsed_dict
+            print("matched!", style)
+            print(len(parsed_dict)-1, "entry files has found!")
+            return parsed_dict
+    return None
 
 def genbib(parsed_dict):
     bib = '%{}\n'.format(parsed_dict.pop('source'))
@@ -89,11 +110,20 @@ if __name__ == "__main__":
                         help="output bibtex filename")
     args = parser.parse_args()
 
+    failed = list()
     with open(args.input, 'r') as f:
         lines = f.readlines()
     o = open(args.output, 'w')
     for line in lines:
         parsed_dict = parse(line)
-        o.write(genbib(parsed_dict))
+        if parsed_dict is not None:
+            o.write(genbib(parsed_dict))
+        else:
+            failed.append(line)
     o.close()
-    print("writed to ", args.output)
+    print("writed to", args.output)
+
+    if len(failed) > 0:
+        print("failed to parse:")
+        for i in failed:
+            print('\t',i)
